50bedd093be498016ebde4921991a4c06c88cd9d
Romek van Boxtel <romek.boxtel@gmail.com>
2019-12-02 09:46:15 +0100

    added the second project for the microwave

---
+++ SoftwareSecurityAssignmentConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareSecurityAssignmentConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
